f25152e79c28226fcc0d4dfca669ccc216796bb0
Dulio Denis <dulio.denis@yahoo.com>
2015-01-20 17:23:26 -0500

    Photo Filters and Sending to Instagram

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -18,6 +18,7 @@
 #import "Constants.h"
 #import "CameraViewController.h"
 #import "ImageLibraryViewController.h"
+#import "PostToInstagramViewController.h"
 
 @interface ImagesTableViewController () <MediaTableViewCellDelegate, UIViewControllerTransitioningDelegate, CameraViewControllerDelegate, ImageLibraryViewControllerDelegate>
 
@@ -421,24 +422,24 @@ forRowAtIndexPath:(NSIndexPath *)indexPath {
 
 
 - (void)cameraViewController:(CameraViewController *)cameraViewController didCompleteWithImage:(UIImage *)image {
-    [cameraViewController dismissViewControllerAnimated:YES completion:^{
-        if (image) {
-            NSLog(@"Captured an image!");
-        } else {
-            NSLog(@"Closed without an image.");
-        }
-    }];
+     [self handleImage:image withNavigationController:cameraViewController.navigationController];
 }
 
 
 - (void)imageLibraryViewController:(ImageLibraryViewController *)imageLibraryViewController didCompleteWithImage:(UIImage *)image {
-    [imageLibraryViewController dismissViewControllerAnimated:YES completion:^{
-        if (image) {
-            NSLog(@"Got an image!");
-        } else {
-            NSLog(@"Closed without an image.");
-        }
-    }];
+     [self handleImage:image withNavigationController:imageLibraryViewController.navigationController];
+}
+
+
+#pragma mark - Handle Image for Posting to Instagram
+
+- (void)handleImage:(UIImage *)image withNavigationController:(UINavigationController *)nav {
+    if (image) {
+        PostToInstagramViewController *postVC = [[PostToInstagramViewController alloc] initWithImage:image];
+        [nav pushViewController:postVC animated:YES];
+    } else {
+        [nav dismissViewControllerAnimated:YES completion:nil];
+    }
 }
 
 @end
---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Dulio Denis on 1/20/15.
+//  Copyright (c) 2015 ddApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+ - (instancetype)initWithImage:(UIImage *)sourceImage;
+
+@end
